39d592d23c2abb5a8855738e4899b9b5ffbd9850
horbel <gorbel.alex@ya.ru>
2016-03-28 20:56:08 +0300

    <a> styles added

--- Mts.WebUI/Controllers/HomeController.cs
+++ Mts.WebUI/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using Mts.Domain;
 using Mts.Domain.Abstract;
+using Mts.WebUI.Models;
 
 namespace Mts.WebUI.Controllers
 {
@@ -17,14 +18,22 @@ namespace Mts.WebUI.Controllers
         }
         public ActionResult Index()
         {
-            
             return View(repository.Products.ToList());
         }
 
         public ViewResult ListByCategory(string category = null)
         {
+            ProductsListViewModel viewModel = new ProductsListViewModel
+            {
+                Products = repository.Products,
+                CurrentCategory = category
+            };
+            ViewBag.SelectedCategory = viewModel.CurrentCategory;
             if (category != null)
+            {
                 return View(repository.Products.Where(x => x.ProductTypes.Name == category).ToList<Products>());
+            }
+                
             else
                 throw new HttpException(404, "not found type of device");
         }
---
+++ Mts.WebUI/Models/ProductsListViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Mts.Domain;
+
+namespace Mts.WebUI.Models
+{
+    public class ProductsListViewModel
+    {
+        public IEnumerable<Products> Products { get; set; }
+        public string CurrentCategory { get; set; }
+    }
+}
